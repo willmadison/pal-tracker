551e9d9244f566dc292d9cd26043ecb4f2fb2eb5
Will Madison <wmadison@Wills-MacBook-Pro.local>
2018-09-11 09:51:06 -0400

    completes PAL cloudNativeDeveloperSimpleApp assingnment

---
+++ src/test/java/io/pivotal/pal/tracker/WelcomeControllerTest.java
@@ -0,0 +1,28 @@
+package io.pivotal.pal.tracker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class WelcomeControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void shouldWelcomeNewcomers() throws Exception {
+        mvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("hello")));
+    }
+}
